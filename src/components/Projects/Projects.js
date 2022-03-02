1e119f3b3c695f4122045e9d3ea430db81ebacf6
MellakSoufiane <mellak.soufiane12@gmail.com>
2022-03-02 20:18:02 +0100

    Update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,12 +3,12 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.jpeg";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+
+import upload from "../../Assets/Projects/Upload.PNG";
+import qcm from "../../Assets/Projects/qcm.png";
+import movie from "../../Assets/Projects/movie.png";
+import powercard from "../../Assets/Projects/Powercard.png";
+
 
 function Projects() {
   return (
@@ -24,64 +24,54 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={upload}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              link="https://github.com/soumyajit4419/Chatify"
+              title="Upload Video Application"
+              description="Functionnality of this application :
+              User should be able to Signup/Login.
+              User should be able to see a list of previously uploaded videos.
+              User should be able to reset his password.
+              User should be able to upload/add new Videos.
+              User should be able to Edit a Video fields after upload.
+              User should be able to Delete his Videos.
+              using : React/Nextjs, Apollo Client, Nodejs, GraphQL(Apollo Server / Express),
+              Heroku, Git, Github.
+              "
+              link="https://github.com/MellakSoufiane/Projet_Innovation"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={powercard}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              link="https://github.com/soumyajit4419/Plant_AI"
+              title="PowerCARD Application"
+              description="Create PowerCARDs profils using Angular Spring Boot and MongoDB. Functionality : Create , List, Delete"
+              link="https://github.com/MellakSoufiane/PowerCard_Application"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={qcm}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              link="https://github.com/soumyajit4419/Editor.io"
+              title="QCM Application"
+              description="A simple QCM using javascripts, html and css"
+              link="https://github.com/MellakSoufiane/QCM-Appli"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={movie}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              link="https://github.com/soumyajit4419/Bits-0f-C0de"
+              title="MicroServices"
+              description="A simple project of movie catalog and rating using spring."
+              link="https://github.com/MellakSoufiane/MicroservicesSpring"
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              link="https://github.com/soumyajit4419/AI_For_Social_Good"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              link="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-            />
-          </Col>
+      
         </Row>
       </Container>
     </Container>
